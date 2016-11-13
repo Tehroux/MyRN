48accb3957ee4ac1fef91716b56bbe3370dca7c4
Phoenix63 <romain.labu@gmail.com>
2016-11-13 16:09:06 +0100

    Debug and generic function added to list_template

--- list/list_template.c
+++ list/list_template.c
@@ -1,10 +1,29 @@
 #include "list_template.h"
 
+/* Basic collections */
+define_list(int)
+
 int main()
 {
 
     List(int) *list = new_list(int);
-    if (list != NULL) free(list);
+    if (list != NULL) {
+        
+        printf("%s\n", (is_empty(list)? "True" : "False"));
+        
+        Element(int) *e = new_element(int, 1);
+        if (e != NULL) {
+        
+            printf("e->value = %d\n", e->value);
+            
+            add(list, e);
+            printf("%s\n", (is_empty(list)? "True" : "False"));
+            
+            free(e);
+        }
+        free(list);
+        
+    }
     
     return 0;
 
