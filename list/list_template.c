0cbd905aa6a22f17995a2e358e57bfec8cf48947
Phoenix63 <romain.labu@gmail.com>
2016-11-14 13:43:22 +0100

    Generic pop function modified Generic clear and destroy function added

--- list/list_template.c
+++ list/list_template.c
@@ -5,25 +5,17 @@ define_list(int)
 
 int main()
 {
-
     List(int) *list = new_list(int);
-    if (list != NULL) {
+    printf("%s\n", (is_empty(list)? "True" : "False"));
         
-        printf("%s\n", (is_empty(list)? "True" : "False"));
-        
-        Element(int) *e = new_element(int, 1);
-        if (e != NULL) {        
-            printf("e->value = %d\n", e->value);            
-            add(list, e);            
-        }
-        
-        printf("%s\n", (is_empty(list)? "True" : "False"));
-        
-        free(pop(list));
-        free(list);
-        
-    }
-    
-    return 0;
+    printf("new_element(int, 1) ->\n");
+    Element(int) *e = new_element(int, 1);
+    if (e != NULL)
+        printf("e->value = %d\n", e->value);    
+                
+    add(list, e);  
+    printf("%s\n", (is_empty(list)? "True" : "False"));
 
+    destroy(list);
+    return 0;    
 }
--- list/list_template.h
+++ list/list_template.h
@@ -9,49 +9,55 @@
         struct _list_##type; \
         struct _list_element_##type; \
         \
-        typedef struct { \
-            \
-            int (*is_empty)(const struct _list_##type*); \
-            size_t (*size)(const struct _list_##type*); \
-            void (*add)(struct _list_##type*, struct _list_element_##type*); \
-            struct _list_element_##type * (*pop)(struct _list_##type*); \
-            \
+        typedef struct \
+        { \
+            int     (*is_empty) (const struct _list_##type*); \
+            size_t  (*size)     (const struct _list_##type*); \
+            void    (*add)      (struct _list_##type*, struct _list_element_##type*); \
+            type    (*pop)      (struct _list_##type*); \
+            void    (*clear)    (struct _list_##type*); \
+            void    (*destroy)  (struct _list_##type*); \
         } _list_functions_##type; \
         \
-        typedef struct _list_element_##type { \
-            \
-            type value; \
+        typedef struct _list_element_##type \
+        { \
+            type                        value; \
             struct _list_element_##type *next; \
-            \
         } list_element_##type; \
         \
-        typedef struct _list_##type { \
+        typedef struct _list_##type \
+        { \
             size_t                  size; \
             list_element_##type     *head; \
             list_element_##type     *queue; \
             _list_functions_##type  *functions; \
-            \
         } list_##type; \
         \
         /* Functions prototype */ \
-        list_##type * new_list_##type(); \
-        list_element_##type * new_element_##type(const type value); \
-        int list_is_empty_##type(const list_##type *list); \
-        size_t list_size_##type(const list_##type *list); \
-        void list_add_##type(list_##type *list, list_element_##type *e); \
-        list_element_##type * list_pop_##type(list_##type *list); \
+        list_##type *           new_list_##type(); \
+        list_element_##type *   new_element_##type(const type value); \
+        int                     list_is_empty_##type(const list_##type *list); \
+        size_t                  list_size_##type(const list_##type *list); \
+        void                    list_add_##type(list_##type *list, list_element_##type *e); \
+        type                    list_pop_##type(list_##type *list); \
+        void                    list_clear_##type(list_##type *list); \
+        void                    list_destroy_##type(list_##type *list); \
         \
         /* Functions defining */ \
-        int list_is_empty_##type(const list_##type *list){ \
+        int list_is_empty_##type(const list_##type *list) \
+        { \
             if (list == NULL) return 1; \
             return list->size == 0; \
         } \
         \
-        size_t list_size_##type(const list_##type *list){ \
+        size_t list_size_##type(const list_##type *list) \
+        { \
             if (list == NULL) return 0; \
             return list->size; \
         } \
-        void list_add_##type(list_##type *list, list_element_##type *e){ \
+        \
+        void list_add_##type(list_##type *list, list_element_##type *e) \
+        { \
             if (list != NULL) { \
                 if (list->head == NULL) { \
                     list->head = e; \
@@ -63,23 +69,45 @@
                 list->size++; \
             } \
         } \
-        list_element_##type * list_pop_##type(list_##type *list){ \
-            if (list == NULL) return NULL; \
+        \
+        type list_pop_##type(list_##type *list) \
+        { \
+            if (list == NULL || list->head == NULL) return 0; \
             list_element_##type * tmp = list->head; \
-            if (tmp == NULL) return NULL; \
+            type v = tmp->value; \
             list->head = list->head->next; \
             list->size--; \
-            return tmp; \
+            free(tmp); \
+            return v; \
+        } \
+        \
+        void list_clear_##type(list_##type *list) \
+        { \
+            int i = 0, n = list_size_##type(list); \
+            for (i=0; i<n; i++) { \
+                list_pop_##type(list); \
+            } \
+        } \
+        \
+        void list_destroy_##type(list_##type *list) \
+        { \
+            if (list != NULL) { \
+                list_clear_##type(list); \
+                free(list); \
+            } \
         } \
         \
         _list_functions_##type _list_func_##type = { \
             &list_is_empty_##type, \
             &list_size_##type, \
             &list_add_##type, \
-            &list_pop_##type \
+            &list_pop_##type, \
+            &list_clear_##type, \
+            &list_destroy_##type \
         }; \
         \
-        list_##type * new_list_##type(){ \
+        list_##type * new_list_##type() \
+        { \
             list_##type *list = (list_##type *) malloc(sizeof(list_##type)); \
             list->size =        0; \
             list->head =        NULL; \
@@ -105,12 +133,13 @@
     #define size(collection) collection->functions->size(collection)
     #define add(collection, element) collection->functions->add(collection, element)
     #define pop(collection) collection->functions->pop(collection)
+    #define clear(collection) collection->functions->clear(collection)
+    #define destroy(collection) collection->functions->destroy(collection)
     
     /*
-       TODO: get(collection, i)
        TODO: pop(collection)
-       TODO: remove(collection, i)
-       TODO: clean(collection)
+       TODO: destroy(collection)
+       TODO: clear(collection)
     */
     
 #endif /* LIST_TEMPLATE_H */
